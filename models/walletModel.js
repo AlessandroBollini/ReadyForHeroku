613d7c37f3a76cfa5c632207b8bb5e2ce6c61347
USERDOMAIN\bollini <bollini.alessandro1998@gmail.com>
2022-08-01 15:21:20 +0200

    aa

--- models/walletModel.js
+++ models/walletModel.js
@@ -1,8 +1,23 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('NFTWallets', 'bollini', 'password', {
-    host: 'localhost',
-    dialect: 'postgres'
+const sequelize = new Sequelize('deom1eaebca2ao', 'btdwzylnjfbpei', 'ad8ef5f7085442f729d7e8809c6f29586b1ac9bc631b95600941ff3aaeccecdc', {
+    host: 'ec2-52-212-228-71.eu-west-1.compute.amazonaws.com',
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: true, 
+          rejectUnauthorized: false
+        }
+      },
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 class Wallet extends Model { }
 Wallet.init({
