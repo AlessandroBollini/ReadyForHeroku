4f817e30e46cc98131681209c40184c913cfaf35
USERDOMAIN\bollini <bollini.alessandro1998@gmail.com>
2022-08-09 17:27:49 +0200

    localdb

--- models/walletModel.js
+++ models/walletModel.js
@@ -1,23 +1,29 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('d5ji12bvim4l88', 'hhevbrrwvawqxx', 'b04b1515b59f8efb0f83a908fd337fd028f315aad2fab8c2a8acd23176ae75ec', {
-    host: 'ec2-34-253-119-24.eu-west-1.compute.amazonaws.com',
+/**const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('d2udoeqr5p34ka', 'lxbbmbefejngpl', 'bf3a21d2eb23330cb20836845e984e73da33c00db7b4afba6db21ff3ebc74354', {
+    host: 'ec2-54-155-110-181.eu-west-1.compute.amazonaws.com',
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
-          require: true, 
-          rejectUnauthorized: false
+            require: true,
+            rejectUnauthorized: false
         }
-      },
+    },
 });
 
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+*/
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('NFTWallets', 'bollini', 'password', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
   
 class Wallet extends Model { }
 Wallet.init({
